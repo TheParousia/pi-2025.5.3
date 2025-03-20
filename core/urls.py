f515563aade68ad3d87225985aee6760e7f7d48c
jamillyoliveira000 <jamillyoliveira839@gmail.com>
2025-03-20 17:54:49 -0300

    Criação do app
    foi criado o app as rotas as views e a página html
    
    Co-Authored-By: Larry53560746 <200672591+Larry53560746@users.noreply.github.com>
    Co-Authored-By: rohsoares <200827324+rohsoares@users.noreply.github.com>

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from usuario import views 
 urlpatterns = [
+    path('',views),
     path('admin/', admin.site.urls),
 ]
