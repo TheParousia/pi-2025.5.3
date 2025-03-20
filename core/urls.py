d7cfc441be46ff4a1364530663f7e55483d31001
Klay <bianca53520836@edu.pa.senac.br>
2025-03-20 17:59:42 -0300

    Atualização de Settings e urls
    Co-Authored-By: Flower891 <200826753+Flower891@users.noreply.github.com>

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from produto import views
 
 urlpatterns = [
+    path('', views.listagemProduto),
     path('admin/', admin.site.urls),
 ]
