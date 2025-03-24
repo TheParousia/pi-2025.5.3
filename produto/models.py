b5d7c5654bcaeab28b1406113ea96d3a5a82a551
Klay <bianca53520836@edu.pa.senac.br>
2025-03-24 14:56:42 -0300

    Atualização de arquivo html e models.py
    Atualização do arquivo listagem_produto para produtos mais dinâmicos e adição de dados ao models
    
    Co-Authored-By: Flower891 <200826753+Flower891@users.noreply.github.com>

--- produto/models.py
+++ produto/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    imagem = models.ImageField(upload_to='produtos/')
+    quantidade_estoque = models.IntegerField()
--- produto/views.py
+++ produto/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Produto
 # Create your views here.
 def listaProdutos(request):
     produtos = Produto.objects.all()
