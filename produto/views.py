318c5ac97a0dddf0f92c786ed32d400ea6a8aad2
Klay <bianca53520836@edu.pa.senac.br>
2025-03-25 18:03:49 -0300

    Melhoras no layout do site
    Adicionamos filtragem de produto, como valor mínimo e máximo, botões no início da página, o nome da loja "Lojão dos 20", e botão de detalhes.

--- produto/views.py
+++ produto/views.py
@@ -2,5 +2,30 @@ from django.shortcuts import render
 from .models import Produto
 # Create your views here.
 def listaProdutos(request):
+    nome_produto = request.GET.get('nome_produto')
+    valor_min = request.GET.get('valor_min')
+    valor_max = request.GET.get('valor_max')
+    ordem = request.GET.get('ordem')
+    
+    print("Produto pesquisado: ", nome_produto)
+    
     produtos = Produto.objects.all()
+    
+    if nome_produto:
+        produtos = Produto.objects.filter(nome__icontains=nome_produto)
+    
+    if valor_min and valor_max:
+        produtos = Produto.objects.filter(preço__range=(valor_min, valor_max))
+
+    if ordem:
+        if ordem == '1':
+            produtos = Produto.objects.all().order_by('nome')
+        elif ordem == '2':
+            produtos = Produto.objects.all().order_by('preço')
+        elif ordem == '3':
+            produtos = Produto.objects.all().order_by('-nome')
+        elif ordem == '4':
+            produtos = Produto.objects.all().order_by('-preço')
+        #  Member.objects.all().order_by('firstname').values()
+
     return render(request, 'listagem_produto.html', {'produtos': produtos})
