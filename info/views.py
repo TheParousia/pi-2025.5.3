81297b76e61528f41c48660222506d3641fb5827
HenriqueDiomedes <j.hyyk77@gmail.com>
2025-03-26 17:58:28 -0300

    Edições, mudanças e manutenção de burgs
    Acrescentamos fotos, frases, e a logo
    
    Co-Authored-By: ana-santos08 <200672446+ana-santos08@users.noreply.github.com>

--- info/views.py
+++ info/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
+from .models import FeedBack
 
 # Create your views here.
+def feedback(request):
+    if request.mother == 'POST':
+        print('Dados recebidos com sucesso!')
+        comentario = request.POST.get('comentario')
+
+        comentarios = FeedBack()
+        comentarios.comentario = comentario
+
+        comentarios.save()
+    return render(request, 'sobre_nos.html')
+
 def sobreNos(request):
     return render(request, 'sobre_nos.html')
 
 
 def desenvolvedores(request):
     return render(request, 'desenvolvedores.html')
+
